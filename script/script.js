42ab5ed8569dc157bd1f3f7a5ef8a04b4fff4fb4
BrauJR <romejor3030@gmail.com>
2022-12-15 15:55:04 -0600

    Se creó el archivo script

---
+++ script/script.js
@@ -0,0 +1,49 @@
+
+var numero = 15;
+var cadenaDeTexto = "Hola!!! Feliz Año Nuevo"
+var verdaderoBoolean = true;
+var indefinido = undefined;
+var nada = null;
+
+let variableLocal = "Casa";
+
+const curp = "EMTE0923RLP093";
+
+console.warn("..........Variables Globales..........");
+console.log("number: " + numero);
+console.log("String: " + cadenaDeTexto);
+console.log("boolean: " + verdaderoBoolean);
+console.log("undefined: " + indefinido);
+console.log("null: " + nada);
+
+console.warn("..........Variable Local..........");
+console.log("let: " + variableLocal);
+
+console.warn("..........Constante..........");
+console.log("CURP: " + curp)
+
+var saludo = "Que onda";
+var nombre = "Jorge";
+
+console.warn("..........Concatenación..........");
+console.log(saludo + " " + nombre)
+
+console.warn("..........Interpolación..........");
+console.log(`${saludo} ${nombre}`);
+
+var numero1 = 90;
+var numero2 = 23;
+
+console.warn("..........Suma..........");
+var suma = numero1 + numero2;
+console.log(`${numero1} + ${numero2} = ${suma}`);
+
+console.warn("..........Comparación..........");
+var comparacion = numero1 === numero2;
+console.log(`${numero1} === ${numero2} es ${comparacion}`)
+
+
+
+
+
+
