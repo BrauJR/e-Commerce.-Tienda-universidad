9d426b6c64fb55aebe0bc464714a6122f9910838
BrauJR <romejor3030@gmail.com>
2023-01-11 22:16:52 -0600

    Se implemento funciones, array y objeto en el script

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-
+// ----------------------------- ACTIVIDAD 5 ----------------------------- 
+/*
 let numero = 15;
 let cadenaDeTexto = "Hola!!! Feliz Año Nuevo"
 let verdaderoBoolean = true;
@@ -36,6 +37,67 @@ console.log(`${numero1} + ${numero2} = ${suma}`);
 console.warn("..........Comparación..........");
 var comparacion = numero1 === numero2;
 console.log(`${numero1} === ${numero2} es ${comparacion}`)
+*/
+
+// ----------------------------- ACTIVIDAD 6 ----------------------------- 
+// Punto 1
+console.log("\t****** FUNCIÓN SUMA ****** ");
+function suma(x, y, z) {
+    let resulado = x + y + z;
+    console.log(`El resultado de ${x} + ${y} + ${z} es igual a ${resulado}`);
+}
+suma(10, 20, 30);
+
+// Punto 2
+console.log("\t****** FUNCIÓN COMPARACIÓN ****** ");
+function comparacion(num1, num2) {
+    if(num1 > num2) {
+        console.log(`${num1} es mayor que ${num2}`);
+    } else if (num2 > num1) {
+        console.log(`${num2} es mayor que ${num1}`);
+    } else {
+        console.log("Los números son iguales");
+    }
+}
+comparacion(90, 700);
+
+// Punto 3
+console.log("\t****** FUNCIÓN FLECHA SUMA ****** ");
+let sumaFlecha = (x, y, z) => console.log(`El resultado de ${x} + ${y} + ${z} es igual a ${x + y + z}`);
+sumaFlecha(30, 20, 10);
+
+console.log("\t****** FUNCIÓN FLECHA COMPARACIÓN ****** ");
+let comparacionFlecha = (num1, num2) => {
+    if(num1 > num2) {
+        console.log(`${num1} es mayor que ${num2}`);
+    } else if (num2 > num1) {
+        console.log(`${num2} es mayor que ${num1}`);
+    } else {
+        console.log("Los números son iguales");
+    }
+}
+comparacionFlecha(900, 700);
+
+// Punto 4
+console.log("\t****** OBJETO ESTUDIANTE ****** ");
+estudiante = {
+    id: "1890POITP",
+    nombre: "Julian",
+    carrera: "Sistemas",
+    edad: 24,
+    nacionalidad: "Mexicana",
+    materias: ["Programación Web", "Algoritmos", "Big Data"]
+};
+let {id, nombre, carrera, edad} = estudiante;
+console.log("El alumno " + nombre + " con matrícula " + id + " tiene " + edad + " años y estudia " + carrera)
+
+// Punto 5
+console.log("\t****** ARREGLO ****** ");
+let arreglo = ["Hola!!!", 11, 2023, [true, false]];
+const [saludo, dia, año] = arreglo;
+console.log(saludo);
+console.log("Hoy es el día " + dia + " de enero");
+console.log("Del año " + año);
 
 
 
